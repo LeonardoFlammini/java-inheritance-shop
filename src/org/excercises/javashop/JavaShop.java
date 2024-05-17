1df66e371328f7dedd9f8a050538249831871c4c
LeonardoFlammini <leonardo98flammini@gmail.com>
2024-05-17 15:16:11 +0200

    JavaShop

--- src/org/excercises/javashop/JavaShop.java
+++ src/org/excercises/javashop/JavaShop.java
@@ -1,2 +1,62 @@
-package org.excercises;public class JavaShop {
+package org.excercises.javashop;
+
+public class JavaShop {
+    public static void main(String[] args) {
+
+    Product jeans = new Product("Levis","Bei Pantaloni",34.5,22);
+    Product tshirt = new Product("Maglietta nera","Nerissima",23.6,10);
+
+
+        System.out.println("Codice Jeans: " + jeans.getCode());
+        System.out.println("Nome Jeans: " + jeans.getName());
+        System.out.println("Descrizione Jeans: " + jeans.getDescription());
+        System.out.println("Prezzo Jeans: " + jeans.getPrice());
+        System.out.println("IVA Jeans: " + jeans.getVat());
+        System.out.println("Prezzo con IVA Jeans: " + jeans.getVattedPrice());
+        System.out.println("Nome completo Jeans: " + jeans.getFullName());
+
+
+        System.out.println("***********");
+
+        System.out.println("Codice T-Shirt: " + tshirt.getCode());
+        System.out.println("Nome T-Shirt: " + tshirt.getName());
+        System.out.println("Descrizione T-Shirt: " + tshirt.getDescription());
+        System.out.println("Prezzo T-Shirt: " + tshirt.getPrice());
+        System.out.println("IVA T-Shirt: " + tshirt.getVat());
+        System.out.println("Prezzo con IVA T-Shirt: " + tshirt.getVattedPrice());
+        System.out.println("Nome completo T-Shirt: " + tshirt.getFullName());
+
+
+        jeans.setName("Gucci");
+        jeans.setDescription("Pantaloni di lusso");
+        jeans.setPrice(120.5);
+        jeans.setVat(24);
+
+
+        tshirt.setName("T-Shirt Bianca");
+        tshirt.setDescription("Bianchissima");
+        tshirt.setPrice(30.6);
+        tshirt.setVat(12);
+
+        System.out.println("Codice Jeans: " + jeans.getCode());
+        System.out.println("Nome Jeans: " + jeans.getName());
+        System.out.println("Descrizione Jeans: " + jeans.getDescription());
+        System.out.println("Prezzo Jeans: " + jeans.getPrice());
+        System.out.println("IVA Jeans: " + jeans.getVat());
+        System.out.println("Prezzo con IVA Jeans: " + jeans.getVattedPrice());
+        System.out.println("Nome completo Jeans: " + jeans.getFullName());
+
+
+        System.out.println("***********");
+
+        System.out.println("Codice T-Shirt: " + tshirt.getCode());
+        System.out.println("Nome T-Shirt: " + tshirt.getName());
+        System.out.println("Descrizione T-Shirt: " + tshirt.getDescription());
+        System.out.println("Prezzo T-Shirt: " + tshirt.getPrice());
+        System.out.println("IVA T-Shirt: " + tshirt.getVat());
+        System.out.println("Prezzo con IVA T-Shirt: " + tshirt.getVattedPrice());
+        System.out.println("Nome completo T-Shirt: " + tshirt.getFullName());
+
+
+    }
 }
--- src/org/excercises/javashop/Product.java
+++ src/org/excercises/javashop/Product.java
@@ -1,2 +1,63 @@
-package org.excercises.javashop;public class Product {
+package org.excercises.javashop;
+
+import java.util.Random;
+
+public class Product {
+
+    Random random = new Random();
+
+    private int code;
+    private String name;
+    private String description;
+    private double price;
+    private double vat;
+
+    //Costruttore
+    public Product(String name,String description,double price,double vat){
+        this.code = random.nextInt(0,999999);
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.vat = vat;
+    }
+
+    //Setter
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public void setVat(double vat){
+        this.vat = vat;
+    }
+
+    //Getter
+    public String getCode(){
+        return String.format("%06d",this.code);
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getDescription(){
+        return this.description;
+    }
+    public double getPrice(){
+        return this.price;
+    }
+    public double getVat(){
+        return this.vat;
+    }
+
+    public double getVattedPrice() {
+        return this.price + (this.price * this.vat / 100);
+    }
+    public String getFullName(){
+        return String.format("%s-%s",this.name,getCode());
+    }
+
+
 }
